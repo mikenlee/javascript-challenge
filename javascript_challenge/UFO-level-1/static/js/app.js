bea59d6af944b0cb6fcb23ee12821f24b918e874
mikenlee <mikeleexp@gmail.com>
2021-01-16 08:52:44 -0500

    added table

--- javascript_challenge/UFO-level-1/static/js/app.js
+++ javascript_challenge/UFO-level-1/static/js/app.js
@@ -2,3 +2,17 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+
+// Get a reference to the table body
+var tbody = d3.select("tbody")
+
+//Refactor to use Arrow Functions!
+tableData.forEach((sighting) => {
+    console.log(sighting);
+    var row = tbody.append("tr");
+    Object.entries(sighting).forEach(([key, value]) => {
+      console.log(value)
+      var cell = row.append("td");
+      cell.text(value);
+    });
+  });
